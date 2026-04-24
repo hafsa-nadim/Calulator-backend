00957047f0ae5ab264159acd89e1ab00a6ede282
RuhamaMalik <malikruhama7@gmail.com>
2026-04-24 13:25:45 +0500

    backend code

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>{
+  console.log('MONGODB CONNECTED')
+})
+.catch((err)=>{
+   console.log(err);
+})
+
+const calcSchema = new mongoose.Schema({
+  expression: String,
+  result: Number
+}) 
+
+const Calc = mongoose.model('Calc' , calcSchema);
+
+app.post('/calculate' , async(req, res)=>{
+   const {expression} = req.body;
+   try{
+    const result = eval(expression);
+    const data = new Calc({expression , result});
+    await data.save();
+    res.json({result});
+   }
+   catch(err){
+    res.json({Error:"invalid Expression"})
+   }
+});
+
+app.get('/history' , async(req , res)=>{
+  const data = await Calc.find();
+  res.json(data)
+})
+
+app.listen(5000, ()=>{
+  console.log('Server Running On Port 5000');
+})
